88217eacb5bce15985900352335812a3c21ed3b6
OleKabigon <mw1166em@gmail.com>
2020-04-17 21:46:14 -0700

    More changes with material css

--- nemoweb/main/views.py
+++ nemoweb/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Tutorial
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse("Homepage-tell me something good")
+    return render(request, template_name="main/home.html", context={"tutorials": Tutorial.objects.all})
